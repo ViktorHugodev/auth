41f7e9783308bc4c8a881b4e5006c24012d59eec
Victor Hugo <viktorhugo.dev@gmail.com>
2022-04-20 21:42:27 -0300

    class page

---
+++ frontend/src/pages/curso/Aulas.tsx
@@ -0,0 +1,11 @@
+import { FeatureMovies } from "../../components/FeatureMovie/FeatureMovies";
+import { LayoutHome } from "../../components/LayoutHome/LayoutHome";
+
+
+export default function Aulas(){
+  return (
+    <LayoutHome>
+      <FeatureMovies/>
+    </LayoutHome>
+  )
+}
